52d63a20b7ef70881f1b719056ad743f6bfc57d7
ronin <roninliu@foxmail.com>
2015-05-18 17:59:44 +0800

    sql file
    sql file

--- DDL/rc_travel.sql
+++ DDL/rc_travel.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2015-05-07 18:01:59
+Date: 2015-05-18 17:59:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,8 +27,12 @@ CREATE TABLE `rc_travel` (
   `is_hot` int(11) NOT NULL,
   `hot_img` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of rc_travel
 -- ----------------------------
+INSERT INTO `rc_travel` VALUES ('1', '豆腐干豆腐干的', '和法国恢复供货', '2015-05-08', '1', null);
+INSERT INTO `rc_travel` VALUES ('2', '对方哈哈哈', '哈哈尽管宏观环境', '2015-05-08', '1', null);
+INSERT INTO `rc_travel` VALUES ('3', '韩国国家', '豆腐干豆腐干', '2015-05-08', '1', null);
+INSERT INTO `rc_travel` VALUES ('4', 'sdfsdfsdf', 'sdfsdfsd', '2015-05-08', '1', null);
--- DDL/rc_user.sql
+++ DDL/rc_user.sql
@@ -10,26 +10,28 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2015-05-18 17:59:24
+Date: 2015-05-18 17:59:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for rc_person
+-- Table structure for rc_user
 -- ----------------------------
-DROP TABLE IF EXISTS `rc_person`;
-CREATE TABLE `rc_person` (
+DROP TABLE IF EXISTS `rc_user`;
+CREATE TABLE `rc_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nickname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `enabled` int(11) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `level` int(11) NOT NULL,
-  `enabled` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of rc_person
+-- Records of rc_user
 -- ----------------------------
+INSERT INTO `rc_user` VALUES ('1', 'admin', 'admin', '1', '测四管理员', 'admin@admin.com', '1234566789');
+INSERT INTO `rc_user` VALUES ('2', 'admin2', 'admin2', '0', 'dsssss', 'asdasd', 'asdas');
